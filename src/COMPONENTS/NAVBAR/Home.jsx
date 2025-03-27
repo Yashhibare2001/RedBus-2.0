ded516ad4a9510491b439d422296bed162203358
Yashhibare2001 <164040628+Yashhibare2001@users.noreply.github.com>
2025-03-27 20:19:53 +0530

    host2

--- src/COMPONENTS/NAVBAR/Home.jsx
+++ src/COMPONENTS/NAVBAR/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
@@ -18,8 +17,6 @@ import barcode from './assest/barcode.jpg';
 
 const Home = () => {
     const [locations, setLocations] = useState([]);
-    // const [selectedLocation, setSelectedLocation] = useState("");
-
     // Fetch Locations from Backend
     useEffect(() => {
         axios.get("http://localhost:5000/api/locations") // Update with your backend API
